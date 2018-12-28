7fa7e2e945a43640ca221b115f4a65c7bfd47555
Victor Dibia <chuvidi2003@gmail.com>
2018-12-27 23:06:26 -0500

    update gneration script and convertion script

---
+++ generate_from_model.py
@@ -0,0 +1,123 @@
+"""
+Generate images using a model checkpoint
+"""
+
+import utils.imutils as imutils
+import tensorflow as tf
+import numpy as np
+import os
+from tensorflow.python.estimator import estimator
+import argparse
+
+import dcgan64_input
+import dcgan64_model
+import dcgan128_input
+import dcgan128_model
+
+
+noise_dim = 100
+current_step = 0
+model = None
+
+
+def noise_input_fn(params):
+    """Input function for generating samples for PREDICT mode.
+
+    Generates a single Tensor of fixed random noise. Use tf.data.Dataset to
+    signal to the estimator when to terminate the generator returned by
+    predict().
+
+    Args:
+      params: param `dict` passed by TPUEstimator.
+
+    Returns:
+      1-element `dict` containing the randomly generated noise.
+    """
+    np.random.seed(20)
+    noise_dataset = tf.data.Dataset.from_tensors(tf.constant(
+        np.random.randn(params['batch_size'], noise_dim), dtype=tf.float32))
+    noise = noise_dataset.make_one_shot_iterator().get_next()
+    return {'random_noise': noise}, None
+
+
+def model_fn(features, labels, mode, params):
+    """Constructs DCGAN from individual generator and discriminator networks."""
+    del labels    # Unconditional GAN does not use labels
+
+    if mode == tf.estimator.ModeKeys.PREDICT:
+        ###########
+        # PREDICT #
+        ###########
+        # Pass only noise to PREDICT mode
+        random_noise = features['random_noise']
+        predictions = {
+            'generated_images': model.generator(random_noise, is_training=False)
+        }
+
+        return tf.contrib.tpu.TPUEstimatorSpec(mode=mode, predictions=predictions)
+
+
+def generate_images(image_size, model_dir, output_dir):
+    global model
+    if(image_size == 64):
+        model = dcgan64_model
+    else:
+        model = dcgan128_model
+
+    config = tf.contrib.tpu.RunConfig(
+        # cluster=tpu_cluster_resolver,
+        model_dir=model_dir)
+
+    _NUM_VIZ_IMAGES = 100
+    cpu_est = tf.contrib.tpu.TPUEstimator(
+        model_fn=model_fn,
+        use_tpu=False,
+        config=config,
+        predict_batch_size=_NUM_VIZ_IMAGES)
+
+    # Render  generated images
+    generated_iter = cpu_est.predict(
+        input_fn=noise_input_fn, checkpoint_path=model_dir)
+    images = [p['generated_images'][:, :, :] for p in generated_iter]
+    assert len(images) == _NUM_VIZ_IMAGES
+
+    image_rows = [np.concatenate(images[i:i+10], axis=0)
+                  for i in range(0, _NUM_VIZ_IMAGES, 10)]
+    tiled_image = np.concatenate(image_rows, axis=1)
+
+    img = imutils.convert_array_to_image(tiled_image)
+
+    step_string = str(current_step).zfill(5)
+    file_obj = tf.gfile.Open(
+        os.path.join(output_dir, 'gen.png'), 'w')
+    img.save(file_obj, format='png')
+    tf.logging.info('Finished generating images')
+
+
+if __name__ == '__main__':
+    tf.logging.set_verbosity(tf.logging.INFO)
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '--model_dir',
+        type=str,
+        default='models/masks/64/model.ckpt-14400',
+        help='Directory containing model checkpoint')
+    parser.add_argument(
+        '--output_dir',
+        type=str,
+        default='models/masks/64/',
+        help='Directory to save generated images')
+    parser.add_argument(
+        '--image_size',
+        type=int,
+        default=64,
+        help='Model to use for generation. Valid values are  [64,128]')
+    # parser.add_argument(
+    #     '--display_images',
+    #     type=int,
+    #     default=0,
+    #     help='To display during conversion')
+
+    args = parser.parse_args()
+    print(args)
+    generate_images(args.image_size, args.model_dir, args.output_dir)
--- utils/convert_to_tfrecords.py
+++ utils/convert_to_tfrecords.py
@@ -14,7 +14,7 @@ import sys
 import io
 from random import shuffle
 
-import utils.imutils as imutils
+import imutils
 import numpy as np
 import tensorflow as tf
 import matplotlib.pyplot as plt
@@ -26,48 +26,6 @@ def _int64_feature(value):
 
 def _bytes_feature(value):
     return tf.train.Feature(bytes_list=tf.train.BytesList(value=[value]))
-
-
-# def convert_folder_tf_sub(data_dir, output_file, display_images, image_size):
-#     index = 0
-#     dir_list = []
-#     dir_count = 0
-#     all_file_list = []
-#     with tf.python_io.TFRecordWriter(output_file) as record_writer:
-#         for root, dirs, filenames in os.walk(data_dir):
-#             for dir in dirs:
-#                 for f in os.listdir(os.path.join(data_dir, dir)):
-#                     file_path = os.path.join(data_dir, dir, f)
-#                     if file_path.split(".")[1] == "jpg" or file_path.split(".")[1] == "png":
-#                         all_file_list.append(file_path)
-#         print("BEfore shuffle", len(all_file_list))
-#         shuffle(all_file_list)
-#         new_list = all_file_list[:3300]
-#         # print((new_list))
-#         for file_path in new_list:
-#             index += 1
-#             image = imutils.get_image(
-#                 file_path, image_size, is_crop=True, resize_w=image_size)
-#             image = imutils.colorize(image)
-#             assert image.shape == (image_size, image_size, 3)
-#             if (image.shape == (image_size, image_size, 3)):
-#                 image += 1.
-#                 image *= (255. / 2.)
-#                 image = image.astype('uint8')
-#                 image_raw = image.tostring()
-#                 if (display_images == 1):
-#                     plt.imshow(image)
-#                     plt.show()
-#                 bytes_feat = _bytes_feature(image_raw)
-#                 print(index, " Writing to tfrecord | ",
-#                       " Image size: ", image_size, " Directory: ", file_path)
-#                 example = tf.train.Example(features=tf.train.Features(feature={
-#                     'image': _bytes_feature(image_raw),
-#                     'label': _int64_feature(dir_count)
-#                 }))
-#                 serialized_example = example.SerializeToString()
-#                 record_writer.write(serialized_example)
-#         print("Done!")
 
 
 def convert_folder_tf(data_dir, output_file, display_images, image_size):
@@ -118,7 +76,7 @@ if __name__ == '__main__':
     parser.add_argument(
         '--output_file',
         type=str,
-        default='images/train.tfrecords',
+        default='images/cifar/train.tfrecords',
         help='Directory to save tfrecords')
     parser.add_argument(
         '--image_size',
